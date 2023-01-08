bdadb168b9efbab58755f712449fd299b35147ce
ngoller <Nicolas.Goller@utah.edu>
2023-01-08 01:11:58 -0800

    fix using ?. where it does not work

--- popup.js
+++ popup.js
@@ -4,8 +4,8 @@ async function updateCount() {
   const result = await chrome.storage.sync.get('count');
   const resultCount = result.count?.standard || 0;
   const rareResultCount = result.count?.rare || 0;
-  document.querySelector('.samoyedCount')?.textContent = `${resultCount}`;
-  document.querySelector('.samoyedRareCount')?.textContent = `${rareResultCount}`;
+  document.querySelector('.samoyedCount').textContent = `${resultCount}`;
+  document.querySelector('.samoyedRareCount').textContent = `${rareResultCount}`;
   const devotion = resultCount + rareResultCount * 10;
   document.querySelector('.totalDevotion').textContent = `${devotion}`;
 }
